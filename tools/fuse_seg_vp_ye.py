388c761e9856afc8f32160d9eb60557245c2fb9d
ShuaiLYU <shuai.lyu@foxmail.com>
2026-02-03 23:01:44 -0600

    Fix:  uisng strip_optimizer to avoid pt["model"] is None

--- tools/fuse_seg_vp_ye.py
+++ tools/fuse_seg_vp_ye.py
@@ -19,114 +19,135 @@ seg_weight=args.seg_weight
 im=args.im
 save_dir=args.save_dir  
 
-os.makedirs(save_dir,exist_ok=True)
+
+def test_yoloe26_vp_func(model,res_im_path):
+
+    end2end=True
+
+    # import os
+    # if not os.path.exists(model):
+    #     raise FileNotFoundError(f"Please download the file '{model}' and place it in the 'weights' directory.")
+
+    import numpy as np
+
+    visual_prompts = dict(
+        bboxes=np.array(
+            [
+                [221.52, 405.8, 344.98, 857.54],  # Box enclosing person
+                [120, 425, 160, 445],  # Box enclosing glasses
+            ],
+        ),
+        cls=np.array(
+            [
+                0,  # ID to be assigned for person
+                1,  # ID to be assigned for glasses
+            ]
+        ),
+    )
+
+    im="../ultralytics/ultralytics/assets/bus.jpg"
+
+    from ultralytics import YOLO,YOLOE
+
+
+
+    if not end2end:
+        del model.model.model[-1].one2one_cv2
+        del model.model.model[-1].one2one_cv3
+        del model.model.model[-1].one2one_cv4
+        model.model.end2end = False
+
+
+    from ultralytics.models.yolo.yoloe import YOLOEVPSegPredictor, YOLOEVPDetectPredictor
+    results = model.predict(
+        im,
+        visual_prompts=visual_prompts,
+        # predictor=YOLOEVPDetectPredictor,
+        predictor=YOLOEVPSegPredictor,
+        conf=0.01,
+    )[0]
+
+
+    import os
+    os.makedirs(os.path.dirname(res_im_path), exist_ok=True)
+    print(f"Image saved to {os.path.abspath(res_im_path)}")
+    results.save(res_im_path)
+
+
+
+# # good style
+# model=YOLOE(seg_weight)
+# model_vp=YOLOE(vp_weight)
+# model.model.model[-1].savpe = model_vp.model.model[-1].savpe
+# model.save(f"weights/yoloe-{scale}-ye.pt")
 
 
 model=YOLO(f"yoloe-{scale}.yaml")
-model.args['clip_weight_name']="mobileclip2:b"
-
-
-# # fafa
 model.load(seg_weight)
+from ultralytics.utils.torch_utils import strip_optimizer
+strip_optimizer(vp_weight)
+model.load(vp_weight)
 
-# model=YOLOE(seg_weight)
 
-# model.load(vp_weight)
-import copy
-model.model.model[-1].savpe=copy.deepcopy(YOLOE(vp_weight).model.model[-1].savpe)
+model.args['clip_weight_name']="mobileclip2:b"
+test_yoloe26_vp_func(model,f"./runs/temp/img1.jpg")
+
+
 
 
 model.save(f"weights/yoloe-{scale}-ye.pt")
-
-
-
-names = ["person", "bus"]
-model.set_classes(names, model.get_text_pe(names))
-# 
-results_seg=model.predict(source=im,conf=0.25,task="segment",save=False)
-results_seg[0].save(filename=f"{save_dir}/segmentation_result.png")
-
-
-import numpy as np
-
-from ultralytics import YOLOE
-from ultralytics.models.yolo.yoloe import YOLOEVPSegPredictor
-
-# Define visual prompts using bounding boxes and their corresponding class IDs.
-# Each box highlights an example of the object you want the model to detect.
-visual_prompts = dict(
-    bboxes=np.array(
-        [
-            [221.52, 405.8, 344.98, 857.54],  # Box enclosing person
-            [120, 425, 160, 445],  # Box enclosing glasses
-        ],
-    ),
-    cls=np.array(
-        [
-            0,  # ID to be assigned for person
-            1,  # ID to be assigned for glasses
-        ]
-    ),
-)
-
-
-
-# Run inference on an image, using the provided visual prompts as guidance
-results = model.predict(
-    im,
-    visual_prompts=visual_prompts,
-    predictor=YOLOEVPSegPredictor,
-    conf=0.01,
-)
-
-# Show results
-import os 
-
-
-
-results[0].save(filename=f"{save_dir}/vp_segmentation_result.png")
-print("Results saved to:", f"{os.path.abspath(save_dir)}/vp_segmentation_result.png")
+model=YOLOE(f"weights/yoloe-{scale}-ye.pt")
+model.args['clip_weight_name']="mobileclip2:b"
+test_yoloe26_vp_func(model,f"./runs/temp/img2.jpg")
 
 
 
 
+###########################################################
+
+
+"""
 
 
 
-# # 26s seg+vp fusion infer test
-#  python tools/fuse_seg_vp.py --scale 26s-seg \
-#  --seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26s-seg_ptwbest_tp_bs256_epo10_close2_engine_yoloe_ye_data.yaml_seg-ultra6/weights/best.pt \
-#   --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26s_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra6\]/weights/best.pt \
-#  --im ultralytics/assets/bus.jpg \
-#  --save_dir runs/merge_vp_seg/
-
-#  # 26n seg+vp fusion infer test
-#     python tools/fuse_seg_vp.py --scale 26n-seg \
-#     --seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26n-seg_ptwbest_tp_bs256_epo10_close2_engine_yoloe_ye_data.yaml_seg-ultra6/weights/best.pt \
-#      --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26n_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra6\]/weights/best.pt \
-#     --im ultralytics/assets/bus.jpg \
-#      --save_dir runs/merge_vp_seg/
+ # 26n seg+vp fusion infer test
+    python runs/tools/fuse_seg_vp_ye.py --scale 26n-seg \
+    --seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26n-seg_ptwbest_tp_bs256_epo10_close2_engine_yoloe_ye_data.yaml_seg-ultra6/weights/best.pt \
+     --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26n_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra6\]/weights/best.pt \
+    --im ultralytics/assets/bus.jpg \
+     --save_dir runs/merge_vp_seg/
 
 
-# # 26m seg+vp fusion infer test
-#  python tools/fuse_seg_vp.py --scale 26m-seg \
-#  --seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26m-seg_ptwbest_tp_bs256_epo10_close2_engine_ye_data_seg-ultra7/weights/best.pt \
-#   --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26m_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra7\]/weights/best.pt \
-#  --im ultralytics/assets/bus.jpg \
-#  --save_dir runs/merge_vp_seg/
-
-#  # 26l seg+vp fusion infer test
-# python tools/fuse_seg_vp_ye.py --scale 26l-seg \
-# --seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26l-seg_ptwbest_tp_bs256_epo10_close2_engine_yoloe_ye_data.yaml_seg-ultra6/weights/best.pt \
-#     --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26l_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra6\]/weights/best.pt \
-# --im ultralytics/assets/bus.jpg \
-#     --save_dir runs/merge_vp_seg/
+# 26s seg+vp fusion infer test
+ python runs/tools/fuse_seg_vp_ye.py --scale 26s-seg \
+ --seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26s-seg_ptwbest_tp_bs256_epo10_close2_engine_yoloe_ye_data.yaml_seg-ultra6/weights/best.pt \
+  --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26s_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra6\]/weights/best.pt \
+ --im ultralytics/assets/bus.jpg \
+ --save_dir runs/merge_vp_seg/
 
 
-# # 26x seg+vp fusion infer test
-#  python tools/fuse_seg_vp.py --scale 26x-seg \
-#  --seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26x-seg_ptwbest_tp_bs256_epo10_close2_engine_yoloe_ye_data.yaml_seg-ultra4/weights/best.pt \
-#   --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26x_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra7\]/weights/best.pt \
-#  --im ultralytics/assets/bus.jpg \
-#  --save_dir runs/merge_vp_seg/  
 
+
+# 26m seg+vp fusion infer test
+ python runs/tools/fuse_seg_vp_ye.py --scale 26m-seg \
+ --seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26m-seg_ptwbest_tp_bs256_epo10_close2_engine_ye_data_seg-ultra7/weights/best.pt \
+  --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26m_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra7\]/weights/best.pt \
+ --im ultralytics/assets/bus.jpg \
+ --save_dir runs/merge_vp_seg/
+
+ # 26l seg+vp fusion infer test
+python runs/tools/fuse_seg_vp_ye.py --scale 26l-seg \
+--seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26l-seg_ptwbest_tp_bs256_epo10_close2_engine_yoloe_ye_data.yaml_seg-ultra6/weights/best.pt \
+    --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26l_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra6\]/weights/best.pt \
+--im ultralytics/assets/bus.jpg \
+    --save_dir runs/merge_vp_seg/
+
+
+# 26x seg+vp fusion infer test
+ python runs/tools/fuse_seg_vp_ye.py --scale 26x-seg \
+ --seg_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_seg/26x-seg_ptwbest_tp_bs256_epo10_close2_engine_yoloe_ye_data.yaml_seg-ultra4/weights/best.pt \
+  --vp_weight /home/louis/ultra_louis_work/ultralytics/weights/yoloe26ye/yoloe26_vp/26x_ptwbest_tp_bs256_epo10_close2_engine_yedata_vp_ye\[ultra7\]/weights/best.pt \
+ --im ultralytics/assets/bus.jpg \
+ --save_dir runs/merge_vp_seg/  
+
+"""
